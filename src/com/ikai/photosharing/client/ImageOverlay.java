2ba3901ebde2624bed0eccc1a962e24422c3ae5a
Ikai Lan <ikai.lan@gmail.com>
2010-08-31 18:07:48 -0700

    Adding ability to delete uploaded images

--- src/com/ikai/photosharing/client/ImageOverlay.java
+++ src/com/ikai/photosharing/client/ImageOverlay.java
@@ -5,7 +5,6 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
--- src/com/ikai/photosharing/server/UserImageServiceImpl.java
+++ src/com/ikai/photosharing/server/UserImageServiceImpl.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.ikai.photosharing.client.services.UserImageService;
 import com.ikai.photosharing.shared.UploadedImage;
@@ -35,8 +38,13 @@ public class UserImageServiceImpl extends RemoteServiceServlet implements
 
 	@Override
 	public void deleteImage(String key) {
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
 		UploadedImageDao dao = new UploadedImageDao();
-		dao.delete(key);
+		UploadedImage image = dao.get(key);
+		if(image.getOwnerId() == user.getUserId()) {
+			dao.delete(key);
+		}
 	}
 
 }
