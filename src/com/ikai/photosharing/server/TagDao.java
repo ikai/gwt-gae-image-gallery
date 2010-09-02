5cba02b87f442b95cfb80883bd552251a3c5731b
Ikai Lan <ikai.lan@gmail.com>
2010-09-01 17:57:21 -0700

    Adding a test for persisting tags

--- src/com/ikai/photosharing/server/TagDao.java
+++ src/com/ikai/photosharing/server/TagDao.java
@@ -1,5 +1,38 @@
 package com.ikai.photosharing.server;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.ikai.photosharing.shared.Tag;
+
 public class TagDao {
 
+	DatastoreService datastore;
+	
+	public TagDao() {
+		datastore = DatastoreServiceFactory.getDatastoreService();
+	}
+	
+	
+	/**
+	 * @param tag The Tag object we want persisted
+	 * @return a String encoded Key for the saved object
+	 */
+	public String put(Tag tag) {
+		Entity tagEntity = new Entity(Tag.class.getSimpleName());
+		
+		// TODO: Make this part of an Entity Group with UploadedImage as a parent
+		tagEntity.setProperty("createdAt", tag.getCreatedAt());
+		tagEntity.setProperty("taggerId", tag.getTaggerId());
+		tagEntity.setProperty("photoKey", tag.getPhotoKey());
+		tagEntity.setUnindexedProperty("body", tag.getBody());
+		
+		
+		Key key = datastore.put(tagEntity);
+		String encodedKey = KeyFactory.keyToString(key);
+		return encodedKey;
+	}
+	
 }
--- test/com/ikai/photosharing/server/TagDaoTest.java
+++ test/com/ikai/photosharing/server/TagDaoTest.java
@@ -1,6 +1,75 @@
 package com.ikai.photosharing.server;
 
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.ikai.photosharing.shared.Tag;
+import com.ikai.photosharing.shared.UploadedImage;
 
 public class TagDaoTest {
 
+	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
+			new LocalDatastoreServiceTestConfig());
+
+	DatastoreService datastore;
+	@Before
+	public void setUp() {
+		helper.setUp();
+		datastore = DatastoreServiceFactory.getDatastoreService();
+		
+	}
+
+	@After
+	public void tearDown() {
+		helper.tearDown();
+	}
+	
+	@Test
+	public void testPut() {
+		
+		TagDao dao = new TagDao();
+		Tag tag = new Tag();
+		tag.setBody("tag body");
+		tag.setCreatedAt(new Date());
+		tag.setPhotoKey("photoKey");
+		tag.setTaggerId("1");
+		
+		String key = dao.put(tag);
+		
+		Key rawKey = null;
+		
+		try {
+			rawKey = KeyFactory.stringToKey(key);
+		} catch (NullPointerException e) {
+			Assert.fail("put() returned null key");
+		}
+		
+		try {
+			Entity entity = datastore.get(rawKey);
+			
+			assertEquals("body not set", tag.getBody(), (String) entity.getProperty("body"));
+			assertEquals("createdAt not set", tag.getCreatedAt(), (Date) entity.getProperty("createdAt"));
+			assertEquals("taggerId not set", tag.getTaggerId(), (String) entity.getProperty("taggerId"));			
+			assertEquals("photoKey not set", tag.getPhotoKey(), (String) entity.getProperty("photoKey"));
+		} catch (EntityNotFoundException e) {
+			Assert.fail("Entity not saved correctly");
+		}
+		
+		
+		
+	}
 }
