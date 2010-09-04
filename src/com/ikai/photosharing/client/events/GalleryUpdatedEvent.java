d3351321ee04719c1fbbb7d551c2017ede3cbac6
Ikai Lan <ikai.lan@gmail.com>
2010-09-03 17:29:12 -0700

    Attribution for event code

--- src/com/ikai/photosharing/client/events/GalleryUpdatedEvent.java
+++ src/com/ikai/photosharing/client/events/GalleryUpdatedEvent.java
@@ -2,6 +2,15 @@ package com.ikai.photosharing.client.events;
 
 import com.google.gwt.event.shared.GwtEvent;
 
+/**
+ * @author Ikai Lan
+ * 
+ * Represents a GalleryUpdatedEvent to fire to the message bus for all listeners.
+ * 
+ *	Inspiration:  
+ * 	http://stackoverflow.com/questions/2951621/gwt-custom-events
+ *
+ */
 public class GalleryUpdatedEvent extends GwtEvent<GalleryUpdatedEventHandler> {
 
     public static Type<GalleryUpdatedEventHandler> TYPE = new Type<GalleryUpdatedEventHandler>();
